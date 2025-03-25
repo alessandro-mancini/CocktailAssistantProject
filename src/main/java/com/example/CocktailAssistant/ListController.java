35107d50e05e3181c7a5039d5961163ad82fced8
alemmanci <alemancini2006@gmail.com>
2025-03-25 10:53:06 +0100

    nn fnzn il filtragg

--- src/main/java/com/example/CocktailAssistant/ListController.java
+++ src/main/java/com/example/CocktailAssistant/ListController.java
@@ -3,9 +3,11 @@ package com.example.CocktailAssistant;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -98,5 +100,15 @@ public class ListController {
                 throw new RuntimeException(e);
             }
         }
+    }
+
+    public void filterByInitial(ActionEvent event){
+        Button pressedButton = (Button) event.getSource();
+        String letter = pressedButton.getText();
+
+        drinks = DrinkSerializator.serializeDrinks(sendRequest("search", "f", letter));
+        Platform.runLater(() -> viewDrinks(drinks));
+
+
     }
 }
